b4d588fec05033dd287372bc9655f3ab8f950539
Elizabeth Kruger <166553686+lizakruger10@users.noreply.github.com>
2025-05-18 09:08:17 -0300

    Update Conexao.java

--- Projeto/src/DAO/Conexao.java
+++ Projeto/src/DAO/Conexao.java
@@ -3,11 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author Elizabeth
  */
 public class Conexao {
-    
+    public Connection getConnection() throws SQLException{
+        Connection conexao = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/usuarios",
+                "postgres", "123456");
+        return conexao;
+    }
 }
