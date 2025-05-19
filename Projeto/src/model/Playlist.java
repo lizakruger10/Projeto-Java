22bd9bddcaeaddca17ec6c583a8b719e49874ee8
Elizabeth Kruger <166553686+lizakruger10@users.noreply.github.com>
2025-05-19 05:55:25 -0300

    Add files via upload

---
+++ Projeto/src/model/Playlist.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Elizabeth
+ */
+
+public class Playlist {
+    private int id;
+    private String nome;
+    private List<Musica> musicas;
+
+    public Playlist(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+        this.musicas = new ArrayList<>();
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public List<Musica> getMusicas() { return musicas; }
+
+    public void adicionarMusica(Musica musica) {
+        musicas.add(musica);
+    }
+
+    public void removerMusica(Musica musica) {
+        musicas.remove(musica);
+    }
+}
